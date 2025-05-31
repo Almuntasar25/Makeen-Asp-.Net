13f88c8fd2b4db49029ef720385636f05780d8bb
almuntasar25 <almuntasar.x@gmail.com>
2025-05-31 20:08:50 +0400

    new file

---
+++ To Do/To Do/To Do/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using To_Do.Models;
+
+namespace To_Do.Context
+{
+    public class ApplicatiDbContext: DbContext
+    {
+        public ApplicatiDbContext(DbContextOptions<ApplicatiDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ToDo> ToDos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
